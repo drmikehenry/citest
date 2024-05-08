c8c1dc71426951b6652da879c01a7ed40d9841ec
Michael Henry <drmikehenry@drmikehenry.com>
2024-05-08 08:11:19 -0400

    More.

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,6 +10,8 @@ echo "::notice file=entrypoint.sh,line=7::$GREETING"
 echo "time=$GITHUB_OUTPUT" >>"$GITHUB_OUTPUT"
 echo "pwd=$(pwd)" >>"$GITHUB_OUTPUT"
 
+pwd >> output.txt
 printenv >> output.txt
+ls >> output.txt
 
 exit 0
